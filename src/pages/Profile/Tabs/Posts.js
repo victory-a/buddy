00a90e0dea1818fd3c95a6933be1edb537ff468d
dev-soks <victoryasokomeh@josla.com.ng>
2020-09-06 20:02:08 +0100

    implemented profile posts view

--- src/pages/Profile/Tabs/Posts.js
+++ src/pages/Profile/Tabs/Posts.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Image, Box } from "@chakra-ui/core";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsReplyFill } from "react-icons/bs";
 
-import { PostsContainer } from "./styles";
-import Post from "../components/Post";
+import maleFB from "assets/male-fb.svg";
+import femaleFB from "assets/female-fb.svg";
+
+import {
+  PostsContainer,
+  PostWrapper,
+  ImageWrapper,
+  PostText,
+  PostDetails,
+  Insights,
+  InsightGroup
+} from "./styles";
 
 const user = {
   id: 3,
@@ -77,6 +90,46 @@ const posts = [
     replies: 7
   }
 ];
+
+const Post = ({ user, post }) => {
+  return (
+    <PostWrapper>
+      <ImageWrapper>
+        <Image
+          rounded="full"
+          src={user?.photo}
+          fallbackSrc={user?.gender === "female" ? femaleFB : maleFB}
+        />
+      </ImageWrapper>
+
+      <PostDetails>
+        <h3>{`${user.firstName} ${user.lastName}` ?? ""}</h3>
+        <PostText>{post.text}</PostText>
+        <Insights>
+          <InsightGroup liked={user.id === post.author}>
+            <Box d="flex">
+              <BsReplyFill size={20} />
+              <p>{post.replies}</p>
+            </Box>
+          </InsightGroup>
+          <InsightGroup liked={user.id === post.author}>
+            {user.id === post.author ? (
+              <Box d="flex">
+                <AiFillHeart size={20} color="#ff0100" />
+                <p>{post.likes}</p>
+              </Box>
+            ) : (
+              <Box d="flex">
+                <AiOutlineHeart size={20} />
+                <p>{post.likes}</p>
+              </Box>
+            )}
+          </InsightGroup>
+        </Insights>
+      </PostDetails>
+    </PostWrapper>
+  );
+};
 
 const Posts = () => {
   return (
